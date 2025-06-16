d96bb9821b0c05e68f4658ed7402c9c2b227f06f
Satyam Choubey <satyam7371@gmail.com>
2025-06-16 23:04:52 +0530

    Creating Todo Api

---
+++ src/main/java/org/example/todoapispring/TodoController.java
@@ -0,0 +1,33 @@
+package org.example.todoapispring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoController {
+
+    private static List<Todo> todoList;
+
+    public TodoController() {
+        todoList = new ArrayList<>();
+
+        todoList.add(new Todo(1,false,"Todo 1",1));
+        todoList.add(new Todo(2,false,"Todo 2",2));
+    }
+
+    @GetMapping("/todos")
+    public List getTodos() {
+        return todoList;
+    }
+
+    @PostMapping("/todos")
+    public Todo creteTodo(@RequestBody Todo newTodo){
+        todoList.add(newTodo);
+        return newTodo;
+    }
+}
